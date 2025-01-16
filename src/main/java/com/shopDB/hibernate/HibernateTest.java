eca000764688a1954e11c907d411d01cb00207a6
Dawid Dębkowski <dawid.debo2.pl@gmail.com>
2025-01-16 22:15:06 +0100

    added a user! 2/2

--- src/main/java/com/shopDB/hibernate/HibernateTest.java
+++ src/main/java/com/shopDB/hibernate/HibernateTest.java
@@ -1,34 +1,38 @@
 package com.shopDB.hibernate;
 
-import com.shopDB.entities.Client;
 import com.shopDB.entities.User;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
-import org.hibernate.mapping.UnionSubclass;
 
 public class HibernateTest {
     public static void main(String[] args) {
-        Session session = HibernateUtil.getSessionFactory().openSession();
-
+        Session session = null;
         Transaction transaction = null;
-        try (session) {
+
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
             transaction = session.beginTransaction();
+
             // Tworzenie nowego użytkownika
             User user = new User();
             user.setLogin("testUser");
             user.setPassword("testPassword");
+            user.setAccType("admin");
 
             session.save(user);
             transaction.commit();
 
             System.out.println("Użytkownik zapisany!");
         } catch (Exception e) {
-            if (transaction != null) {
-                transaction.rollback();
+            if (transaction != null && transaction.isActive()) {
+                transaction.rollback(); // Rollback tylko gdy transakcja jest aktywna
             }
             e.printStackTrace();
         } finally {
-            HibernateUtil.shutdown();
+            if (session != null && session.isOpen()) {
+                session.close(); // Zamknięcie sesji ręcznie
+            }
+            HibernateUtil.shutdown(); // Zamknięcie SessionFactory na końcu
         }
     }
 }
