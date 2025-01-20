dc972c6117de24f5e6e0895f13ea0d8354249c7e
Dawid Dębkowski <dawid.debo2.pl@gmail.com>
2025-01-20 22:28:24 +0100

    password layout

--- src/main/java/com/shopDB/controller/LoginController.java
+++ src/main/java/com/shopDB/controller/LoginController.java
@@ -1,7 +1,47 @@
 package com.shopDB.controller;
 
+import io.github.palexdev.materialfx.controls.MFXButton;
+import io.github.palexdev.materialfx.controls.MFXPasswordField;
 import io.github.palexdev.materialfx.controls.MFXTextField;
+import io.github.palexdev.mfxcore.controls.Text;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class LoginController {
-    public MFXTextField usernameText;
+
+    @FXML
+    private VBox fieldsBox;
+
+    @FXML
+    private MFXButton loginButton;
+
+    @FXML
+    private BorderPane mainPane;
+
+    @FXML
+    private MFXPasswordField passwordText;
+
+    @FXML
+    private MFXButton registerButton;
+
+    @FXML
+    private Text titleText;
+
+    @FXML
+    private MFXTextField usernameText;
+
+    @FXML
+    void onLoginButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onRegisterButtonClick(ActionEvent event) {
+
+    }
+
 }
