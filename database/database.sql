864a8b853a1805d740662d0189f168dc1ea92ea0
Błażej Pawluk <bpawluk05@gmail.com>
2025-01-25 13:06:04 +0100

    database: views fix

--- database/database.sql
+++ database/database.sql
@@ -1182,13 +1182,13 @@ delimiter ;
 
 create view ind_client_view as (
 	SELECT c.name, c.surname, c.email, c.phone, a.street, a.house_number, a.apartment_number, a.city, a.postal_code
-	FROM clients c JOIN addresses a ON c.address_id = a.address_id;
-)
+	FROM clients c JOIN addresses a ON c.address_id = a.address_id
+);
 
 create view com_client_view as (
 	SELECT c.company_name, c.email, c.phone, c.NIP, a.street, a.house_number, a.apartment_number, a.city, a.postal_code
-	FROM clients c JOIN addresses a ON c.address_id = a.address_id;
-)
+	FROM clients c JOIN addresses a ON c.address_id = a.address_id
+);
 
 create view product_view as (
 	SELECT p.name, p.category, t.type, c.name AS color, p.price, COALESCE(p.discount, 0) AS discount
