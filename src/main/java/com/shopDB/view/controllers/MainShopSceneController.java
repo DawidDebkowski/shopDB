496c0d2164c24fa61bd2f8063080e23bb1ed3810
Dawid Dębkowski <dawid.debo2.pl@gmail.com>
2025-01-27 23:27:50 +0100

    gui: main shop type select

--- src/main/java/com/shopDB/view/controllers/MainShopSceneController.java
+++ src/main/java/com/shopDB/view/controllers/MainShopSceneController.java
@@ -1,21 +1,23 @@
 package com.shopDB.view.controllers;
 
+import com.shopDB.view.components.SelectTypeLabel;
 import org.springframework.stereotype.Controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
 @Controller
-
 public class MainShopSceneController implements SceneController {
-
-    @FXML
-    private BorderPane mainPane;
-
     @FXML
     private VBox typesWrapper;
 
+    private Collection<SelectTypeLabel> selectTypeLabels;
     private String selectedType;
     private String selectedCategory;
     private String selectedColor;
@@ -23,8 +25,37 @@ public class MainShopSceneController implements SceneController {
     private int minPrice;
     private int maxPrice;
 
-    void initialize() {
+    public void initialize() {
 
+
+        // przykładowa lista z bazy
+        List<String> types = Arrays.asList("spodnie", "koszulka", "bluza", "sukienka");
+        setupTypeLabels(types);
+    }
+
+    /**
+     * Wyświetlenie i
+     * Logika wybierania, tylko 1 możliwe na raz
+     * Utworzenie wszystkich wymaganych label
+     */
+    private void setupTypeLabels(List<String> types) {
+        selectTypeLabels = new ArrayList<>();
+        for (String type : types) {
+            SelectTypeLabel label = new SelectTypeLabel(type);
+            selectTypeLabels.add(label);
+            typesWrapper.getChildren().add(label);
+        }
+
+        for (SelectTypeLabel selectTypeLabel : selectTypeLabels) {
+            selectTypeLabel.setOnMouseClicked(event -> {
+                System.out.println("Clicked on type: " + selectTypeLabel.getText());
+                selectedType = selectTypeLabel.getType();
+                for (SelectTypeLabel otherSelectTypeLabel : selectTypeLabels) {
+                    otherSelectTypeLabel.select(false);
+                }
+                selectTypeLabel.select(true);
+            });
+        }
     }
 
     @FXML
