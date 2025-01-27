03283519bfeb021aa99517ba93a8d2af6bb7c04c
Dawid Dębkowski <dawid.debo2.pl@gmail.com>
2025-01-27 23:27:50 +0100

    gui: main shop type select

--- src/main/java/com/shopDB/view/components/SelectTypeLabel.java
+++ src/main/java/com/shopDB/view/components/SelectTypeLabel.java
@@ -1,12 +1,38 @@
 package com.shopDB.view.components;
 
 import io.github.palexdev.mfxcore.controls.Label;
+import javafx.geometry.Insets;
 
 public class SelectTypeLabel extends Label {
-    private String type;
+    private final String type;
     private boolean selected;
 
-    public SelectTypeLabel() {
+    public SelectTypeLabel(String type) {
+        this.type = type;
+        this.setText(type);
+        selected = false;
 
+        setOnMouseEntered(event -> {
+            clickStyle(true);
+//            System.out.println("Hover over " + getText());
+        });
+        setOnMouseExited(event -> {
+            clickStyle(selected);
+//            System.out.println("Hover over " + getText());
+        });
+    }
+
+    public void select(boolean selected) {
+        clickStyle(selected);
+        this.selected = selected;
+//        System.out.println(type + " selected: " + selected);
+    }
+
+    private void clickStyle(boolean selected) {
+        this.setUnderline(selected);
+    }
+
+    public String getType() {
+        return type;
     }
 }
