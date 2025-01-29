3df02bfc8a1d1421a0e7e9290b01a9a5677704c2
Dawid Dębkowski <dawid.debo2.pl@gmail.com>
2025-01-29 19:21:29 +0100

    gui: cart init

--- src/main/java/com/shopDB/view/controllers/CartSceneController.java
+++ src/main/java/com/shopDB/view/controllers/CartSceneController.java
@@ -1,13 +1,26 @@
 package com.shopDB.view.controllers;
 
+import com.shopDB.dto.OrderDetailDTO;
+import com.shopDB.view.components.SmallProduct;
+import io.github.palexdev.materialfx.controls.MFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.geometry.Pos;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 import org.springframework.stereotype.Controller;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
 
 // lista produktów + usuwanie
 // złóż zamówienie
 
 @Controller
 public class CartSceneController implements SceneController {
-
+    @FXML
+    private VBox cartWrapper;
 
     public void initialize() {
 
@@ -16,5 +29,19 @@ public class CartSceneController implements SceneController {
     @Override
     public void refresh() {
 
+        List<OrderDetailDTO> orderDetailDTOS = Arrays.asList(
+                OrderDetailDTO.getMockWithName("mega kutka", 9.99),
+                OrderDetailDTO.getMockWithName("taka se kutka", 18.00)
+        );
+
+        for (OrderDetailDTO orderDetailDTO : orderDetailDTOS) {
+            SmallProduct smallProduct = new SmallProduct(orderDetailDTO);
+            cartWrapper.getChildren().add(smallProduct);
+        }
+    }
+
+    @FXML
+    private void placeOrder(ActionEvent actionEvent) {
+        // zplejsuj order
     }
 }
