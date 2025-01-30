78fa9b08b39b369267f2c984d6b570c956140e58
Dawid Dębkowski <dawid.debo2.pl@gmail.com>
2025-01-30 12:24:08 +0100

    gui: adding products init

---
+++ src/main/java/com/shopDB/view/controllers/AddProductController.java
@@ -0,0 +1,12 @@
+package com.shopDB.view.controllers;
+
+import org.hibernate.annotations.ConcreteProxy;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class AddProductController implements SceneController {
+    @Override
+    public void refresh() {
+
+    }
+}
