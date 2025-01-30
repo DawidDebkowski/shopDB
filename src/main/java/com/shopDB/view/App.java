115fcbc815a0685074427c0740e4753ac8845c35
Dawid Dębkowski <dawid.debo2.pl@gmail.com>
2025-01-30 12:51:26 +0100

    gui: adding products scene

--- src/main/java/com/shopDB/view/App.java
+++ src/main/java/com/shopDB/view/App.java
@@ -48,7 +48,7 @@ public class App extends Application
         SceneManager sceneManager = springContext.getBean(SceneManager.class);
         sceneManager.initialize(primaryStage);
 
-        SceneManager.getInstance().setScene(SceneType.MAIN_SHOP);
+        SceneManager.getInstance().setScene(SceneType.ADD_PRODUCT);
         primaryStage.setTitle("Shop Application");
         primaryStage.show();
     }
